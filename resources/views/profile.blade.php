89cba845001e39cfdda8cecb363a0aee2d78f6b2
Emmanuel Bassey <leoemmanuel11@gmail.com>
2019-05-06 15:52:25 -0700

    second commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -83,6 +83,9 @@
     <section class="content container-fluid">
 
         <p class="mt-5"></p>
+        @foreach($errors->all() as $error)
+        <p class="alert alert-danger">{{$error}}</p>
+        @endforeach
 
     @if ($message = Session::get('success'))
 <div class="alert alert-success alert-block">
